f8c76a3880a203f117f9d58885cb6fd2964c8264
Leonardo F. Mercado Benitez <lfmercadobe@unal.edu.co>
2021-12-06 20:35:58 -0500

    Se modifica la función de recibir datos para recibir los tres datos nuevos, a saber, tiempo de disparo 1, tiempo de delay entre disparos, tiempo de disparo 2

--- main/main.ino
+++ main/main.ino
@@ -2,10 +2,11 @@
  * 
  * Autor: Leonardo Fabio Mercado Benítez.
  * 
- * Versión: 0.1
+ * Versión: 0.2
  * 
  * Conexiones: 
- *  - Pin  D2: Salida de control para driver del IGBT. 
+ *  - Pin D2: Salida de control para el driver del IGBT 1.
+ *  - Pin D3: Salida de control para el driver del IGBT 2.
  */
  
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -14,13 +15,18 @@
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //- - - - - - - - VARIABLES - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-int disparo = 2;            // Definición del puerto GPIO de disparo.
-int tiempo_disparo = 0;     // Variable para almacenar el tiempo de disparo.
-int led = 13;               // Definición del led testico para pruebas.
-int valor;                  // Variable para almacenar el valor dirigido al objetivo.
-String objetivo = "";       // Cadena que almacena el tipo de señal entrante por el bluetooth.
-String valor_auxiliar = ""; // Almacena temporalmente el valor entrante en forma de cadena.
-String cadena = "";         // Almacena toda la cadena proveniente del bluetooth.
+int disparo = 2;              // Definición del puerto GPIO de disparo de 1.
+int disparo_2 = 3;              // Definición del puerto GPIO de disparo de 2.
+int tiempo_disparo_1 = 0;     // Variable para almacenar el tiempo de disparo 1.
+int delay_disparos = 0;       // Variable para almacenar el delay entre disparos.
+int tiempo_disparo_2 = 0;     // Variable para almacenar el tiempo de disparo 2.
+int led = 13;                 // Definición del led testico para pruebas.
+int valor;                    // Variable para almacenar el valor dirigido al objetivo.
+String objetivo = "";         // Cadena que almacena el tipo de señal entrante por el bluetooth.
+String valor_auxiliar_1 = ""; // Almacena temporalmente el valor entrante en forma de cadena {Tiempo disparo 1}.
+String valor_auxiliar_2 = ""; // Almacena temporalmente el valor entrante en forma de cadena {Delay entre disparos}.
+String valor_auxiliar_3 = ""; // Almacena temporalmente el valor entrante en forma de cadena {Tiempo disparo 2}.
+String cadena = "";           // Almacena toda la cadena proveniente del bluetooth.
 
 void setup(){ 
   Serial.begin(38400);      // Inicialización del objeto para la comunicación serial.
@@ -58,11 +64,20 @@ void recibirMensaje(){
     cadena = cadena + char(Serial.read());                    // Concatena el mensaje y lo almacena en cadena.
     delay(1);                                                 // Delay de estabilidad.
   }  
-  if (cadena.indexOf(" ") >=0){                               // Comprobación del formato del mensaje correcto. [objetivo[espacio]valor]
-    objetivo = cadena.substring(0,(cadena.indexOf(" ")));     // Filtra el objetivo de la cadena.
-    valor_auxiliar = cadena.substring(cadena.indexOf(" ")+1); // Filtra el valor de la cadena.
-    valor = valor_auxiliar.toInt();                           // Convierte el valor del objetivo a entero.
-    //Serial.println(objetivo);                               // Para debug - Comprobación del objeto recibido.
-    //Serial.println(valor);                                  // Para debug - Comprobación del valor para el objeto recibido.
+  if (cadena.indexOf(",") >=0){                               // Comprobación del formato del mensaje correcto. [objetivo[coma]valor]
+    objetivo = cadena.substring(0,(cadena.indexOf(",")));     // Filtra el objetivo de la cadena.
+    int aux_v1 = cadena.indexOf(",")+1;
+    valor_auxiliar_1 = cadena.substring(aux_v1,cadena.indexOf(",",aux_v1)); // Filtra el valor 1 de la cadena.
+    int aux_v2 = cadena.indexOf(",",aux_v1)+1;
+    valor_auxiliar_2 = cadena.substring(aux_v2,cadena.indexOf(",",aux_v2)); // Filtra el valor 1 de la cadena.
+    int aux_v3 = cadena.indexOf(",",aux_v2)+1;
+    valor_auxiliar_3 = cadena.substring(aux_v3); // Filtra el valor 1 de la cadena.
+    tiempo_disparo_1 = valor_auxiliar_1.toInt();                           // Convierte el valor del objetivo a entero.
+    delay_disparos = valor_auxiliar_2.toInt();
+    tiempo_disparo_2 = valor_auxiliar_3.toInt();
+    Serial.println(objetivo);                               // Para debug - Comprobación del objeto recibido.
+    Serial.println(tiempo_disparo_1);                                  // Para debug - Comprobación del valor para el objeto recibido.
+    Serial.println(delay_disparos);                                  // Para debug - Comprobación del valor para el objeto recibido.
+    Serial.println(tiempo_disparo_2);                                  // Para debug - Comprobación del valor para el objeto recibido.
   }
 }
